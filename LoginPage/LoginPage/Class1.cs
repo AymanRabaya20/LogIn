198cd8de23e94d2b9f66c3692c84115259ed83d4
Ayman Rabaya <aymanwaleedrabaya@gmail.com>
2021-07-29 19:04:51 +0300

    The project

---
+++ LoginPage/LoginPage/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    class User
+    { 
+         public string name;
+         public string password;
+         public string email;
+        public User (string name,string password,string email)
+        {
+            this.name = name;
+            this.password = password;
+            this.email = email;
+        }
+        public string getInfo()
+        {
+            return "User : { name " + name + " password " + password + " email " + email;
+        }
+    }
+}
---
+++ LoginPage/LoginPage/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace LoginPage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IFirebaseConfig fbc = new FirebaseConfig()
+        {
+            BasePath = "https://test2-7a7b6-default-rtdb.firebaseio.com/",
+            AuthSecret = "pt19h6SXqluVcSdE9n2zomihBcgeXSvMjSm3jBtq",
+        };
+        IFirebaseClient cln;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cln = new FireSharp.FirebaseClient(fbc);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Check your Connection !");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                User u = cln.Get("Users/" + textBox1.Text).ResultAs<User>();
+                if (u != null)
+                {
+                    if (u.password == textBox2.Text && u.email == textBox3.Text)
+                    {
+                        MessageBox.Show("Welcome");
+                    }
+                    else MessageBox.Show("Wrong Password or email");
+                }
+                else MessageBox.Show("There is no such a user with this name ! \n");
+                
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Check your counction !");
+            }
+        }
+    }
+}
